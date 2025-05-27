75f602b6809dc8d2e41f72abcc0f79f3a81d96c5
johancube <johan@cube.dev>
2025-05-27 20:20:22 +0300

    fix: new chat

--- a2a-react/app/api/stream-chat-state/route.ts
+++ a2a-react/app/api/stream-chat-state/route.ts
@@ -26,7 +26,6 @@ export async function POST(request: NextRequest) {
   const requestBody: any = {
     chatId: String(chatId),
     cubeCredentials: {
-      __typename: 'AIEngineerCubeCredentialsDTO',
       apiToken: apiToken,
       apiUrl: apiUrl,
     },
